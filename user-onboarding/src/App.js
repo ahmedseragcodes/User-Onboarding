7f22d44ec65759e1cdcc46065259f20d5ef72519
ahmedseragcodes <ahmedserag1@gmail.com>
2020-11-12 10:39:46 -0500

    added name, email, pass fields, as well as checkbox for terms of service and submit button

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,28 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import Form from "./Form";
 
 function App() {
+
+const [form, setForm]=useState({
+  name: "",
+  email: "",
+  password: "",
+})
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label htmlFor="name">Name:
+          <input type="text" name="name" value={form.name} />
+        </label>
+        <label htmlFor="email">Email:
+          <input type="email" name="email" value={form.email} />
+        </label>
+        <label htmlFor="password">Password:
+          <input type="password" name="password" value={form.password} />
+        </label>
+        <label htmlFor="termsOfService">
+          <input type="checkbox" name="termsOfService" checked={true} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
