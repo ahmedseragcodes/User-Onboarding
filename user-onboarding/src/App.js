d61993b8ca2d8ec39c8f5537793e22303ca9bd8a
ahmedseragcodes <ahmedserag1@gmail.com>
2021-01-27 15:39:24 -0500

    setup required slices of state and onchange and onsubmit functions

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -3,10 +3,32 @@ import React, { useState } from "react";
 import Form from "./components/form";
 import styled from "styled-components";
 
+const initialFormValues={
+  name: "",
+  email: "",
+  password: "",
+  termsOfService: false
+}
+
+const initialFormErrors={
+  name: "",
+  email: "",
+  password: ""
+}
+const initialUsers=[];
+const initialDisabled=true;
+
 function App() {
+
+  const [users, setUsers]=(initialUsers);
+  const [formValues, setFormValues]=useState(initialFormValues);
+  const [formErrors, setFormErrors]=useState(initialFormErrors);
+  const [disabled, setDisabled]=useState(initialDisabled);
+
+
   return (
     <UserOnboarding>
-      <Form />
+      <Form values={formValues} setFormValues={setFormValues} errors={formErrors} users={users} setUsers={setUsers} disabled={disabled} setFormErrors={setFormErrors} setDisabled={setDisabled} initialFormValues={initialFormValues} />
     </UserOnboarding>
   );
 }
--- user-onboarding/src/components/form.js
+++ user-onboarding/src/components/form.js
@@ -1,10 +1,54 @@
+import axios from "axios";
 import React, { useState } from "react";
 import styled from "styled-components";
 
+// const initialFormValues={
+//     name: "",
+//     email: "",
+//     password: "",
+//     termsOfService: false
+//   }
+
+
 export default function Form(props){
+
+    const { values, setFormValues, errors, setFormErrors, users, setUsers, disabled, setDisabled, initialFormValues }=props;
+
+const onChange=function(event){
+    const { value, type, name, checked }=event.target;
+
+    const valueToUse=type==="checkbox" ? checked : value;
+
+    setFormValues({...values, [name]: valueToUse})    
+}
+
+const onSubmit=function(event){
+    event.preventDefault();
+
+    const newFriend={
+        name: values.name.trim(),
+        email: values.email.trim(),
+        password: values.password.trim(),
+        termsOfService: values.termsOfService
+    }
+
+    axios.post("https://reqres.in/", newFriend)
+    .then(function(res){
+        setUsers([...users, newFriend]);
+        setFormValues(initialFormValues);
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+}
+
+
     return (
-        <form>
-            
+        <form onSubmit={onSubmit} >
+            <label htmlFor="name">Name:
+                <input name="name" type="text" value={values.name} onChange={onChange} />
+            </label>
+            <button>Submit</button>
         </form>
     )
 }
